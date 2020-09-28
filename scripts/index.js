c3465b45f9d89f68a20b89ab2ffb354c287c14e2
EricksonSterling <ericksterlingp@gmail.com>
2020-09-28 09:28:24 +0200

    actualización

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,15 @@
 'use strict'
 
+var frase = window.prompt('Introduce una frase: ');
 
+if ( frase === '' ){
+
+    console.error( 'Introduce una frase.' );
+} else {
+
+    console.log( letras(frase) + ' letras y ' + palabras(frase) + ' palabras.' + '\n' +
+        maysc(frase) + '\n' + titulo(frase) + '\n' + letrasReves(frase) + '\n' + palindromo(frase)
+    
+    );
+    
+}
