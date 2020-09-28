60c7b975114db8b01adbec6014931b3a6d34366d
EricksonSterling <ericksterlingp@gmail.com>
2020-09-28 07:55:28 +0200

    Asignando parámetros a las funciones y añadiendo una comprobación en index.js.

--- scripts/functions.js
+++ scripts/functions.js
@@ -1,3 +1,40 @@
 'use strict'
 
+'use strict'
+
+function letras() {
+
+    return num_letras;
+}
+
+function palabras(){
+
+
+    return num_palabras;
+}
+
+function maysc() {
+
+    return mayusculas;
+}
+
+function titulo() {
+
+    return tituloA;
+}
+
+function letrasReves() {
+
+    return letra_alreves;
+}
+
+function palabrasReves() {
+
+    return palabra_alreves;
+}
+
+function palindromo() {
+
+    return true;
+}
 
