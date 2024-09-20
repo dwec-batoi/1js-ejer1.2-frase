4d3f19002fd6db240069f86f540043657ddf2a59
Juan Segura <juanseguravasco@gmail.com>
2024-09-20 17:09:07 +0200

    Update functions.test.js

--- test/functions.test.js
+++ test/functions.test.js
@@ -16,11 +16,11 @@ describe('Funciones sobre la frase "Una frase para probar"', () => {
     test('titleString devuelve Una Frase Para Probar ', () => {
       expect(funciones.titleString(frase)).toBe('Una Frase Para Probar');
     });
-    test('letrasReves devuelve raborp arap esarf anU', () => {
-      expect(funciones.letrasReves(frase)).toBe('raborp arap esarf anU');
+    test('backwardsLetters devuelve raborp arap esarf anU', () => {
+      expect(funciones.backwardsLetters(frase)).toBe('raborp arap esarf anU');
     });
-    test('backwardsLetters devuelve probar para frase Una', () => {
-      expect(funciones.backwardsLetters(frase)).toBe('probar para frase Una');
+    test('backwardsWords devuelve probar para frase Una', () => {
+      expect(funciones.backwardsWords(frase)).toBe('probar para frase Una');
     });
     test('palindrome sobre "Una frase para probar"  devuelve FALSE', () => {
       expect(funciones.palindrome(frase)).toBe(false);
